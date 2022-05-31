d0ecd5b101d924f9035aca2b85ee95d6cb613628
SomGrap <ssomgrap@gmail.com>
2022-05-31 17:50:57 +0200

    update

--- src/pylogs/test.py
+++ src/pylogs/test.py
@@ -1,9 +1,3 @@
 import __init__ as pylogs
 
 logger = pylogs.Logger("Test", r"C:\Users\sacha\Documents\Developpement\Projets\PyLogs\src\pylogs")
-
-logger.colorization(True)
-logger.terminalOutput(True)
-logger.fileOutput(True)
-
-logger.info("hello")
